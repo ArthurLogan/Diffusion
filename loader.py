242026c0d03b197dab4f645eb1d0de2cd23915f3
Logan <lg000601@163.com>
2023-01-19 11:59:51 +0800

    update code

--- loader.py
+++ loader.py
@@ -1,4 +1,3 @@
-import torch
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from torchvision.datasets import CIFAR10
--- train.py
+++ train.py
@@ -1,5 +1,4 @@
 import argparse
-from tqdm import tqdm
 from loader import load_dataset
 
 def parse():
